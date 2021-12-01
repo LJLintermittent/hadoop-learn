74848ef7f6e19cf52116c427bc4f774323706e7d
lijiale <1157221853@qq.com>
2021-12-01 10:29:58 +0800

    HDFS API测试

--- src/main/java/com/learn/hdfs/HdfsClient.java
+++ src/main/java/com/learn/hdfs/HdfsClient.java
@@ -1,8 +1,7 @@
 package com.learn.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +9,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 
 /**
  * Description:
@@ -50,14 +50,58 @@ public class HdfsClient {
     @Test
     public void testPut() throws IOException {
         fileSystem.copyFromLocalFile(true, true
-                , new Path("D:\\hadoop/testPut.txt")
-                , new Path("/java/test3"));
+                , new Path("D:\\hadoop-learn/test.txt")
+                , new Path("/"));
     }
 
     @Test
     public void testGet() throws IOException {
         fileSystem.copyToLocalFile(false, new Path("/java/test2/testPut.txt")
                 , new Path("D:\\"), true);
+    }
+
+    @Test
+    public void testDelete() throws IOException {
+        fileSystem.delete(new Path("/sanguo"), true);
+    }
+
+    @Test
+    public void testMove() throws IOException {
+        // rename可以修改文件名称，目录名称，以及移动文件位置并且修改文件名称
+        fileSystem.rename(new Path("/sanguo"), new Path("/zhanguo"));
+    }
+
+    @Test
+    public void testFileDetail() throws IOException {
+        RemoteIterator<LocatedFileStatus> iterator = fileSystem.listFiles(new Path("/"), true);
+        while (iterator.hasNext()) {
+            LocatedFileStatus fileStatus = iterator.next();
+            System.out.println("当前文件的文件路径：" + fileStatus.getPath());
+            System.out.println(fileStatus.getPermission());
+            System.out.println(fileStatus.getOwner());
+            System.out.println(fileStatus.getGroup());
+            System.out.println(fileStatus.getLen());
+            System.out.println(fileStatus.getModificationTime());
+            System.out.println(fileStatus.getReplication());
+            System.out.println(fileStatus.getBlockSize());
+            System.out.println(fileStatus.getPath().getName());
+            BlockLocation[] blockLocations = fileStatus.getBlockLocations();
+            System.out.println(Arrays.toString(blockLocations));
+
+        }
+    }
+
+    @Test
+    public void testFileOrCatalog() throws IOException {
+        FileStatus[] fileStatuses = fileSystem.listStatus(new Path("/"));
+        for (FileStatus fileStatus : fileStatuses) {
+            if (fileStatus.isFile()) {
+                System.out.println("文件：" + fileStatus.getPath().getName());
+            } else {
+                System.out.println("目录：" + fileStatus.getPath().getName());
+            }
+        }
+
     }
 
 }
