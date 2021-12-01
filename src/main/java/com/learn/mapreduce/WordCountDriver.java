472439118dc55c554dc140a92fc18942db3c2b52
lijiale <1157221853@qq.com>
2021-12-01 22:01:46 +0800

    mapreduce

---
+++ src/main/java/com/learn/mapreduce/WordCountDriver.java
@@ -0,0 +1,13 @@
+package com.learn.mapreduce;
+
+/**
+ * Description:
+ * date: 2021/12/1 21:47
+ * Package: com.learn.mapreduce
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class WordCountDriver {
+}
---
+++ src/main/java/com/learn/mapreduce/WordCountReducer.java
@@ -0,0 +1,15 @@
+package com.learn.mapreduce;
+
+import org.apache.hadoop.mapreduce.Reducer;
+
+/**
+ * Description:
+ * date: 2021/12/1 21:47
+ * Package: com.learn.mapreduce
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class WordCountReducer extends Reducer {
+}
