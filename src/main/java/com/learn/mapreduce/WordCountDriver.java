7909922b6475881e97129c996abe035ccc774a5d
lijiale <1157221853@qq.com>
2021-12-04 21:39:09 +0800

    wordCount Demo放入hadoop集群测试

--- src/main/java/com/learn/mapreduce/WordCountDriver.java
+++ src/main/java/com/learn/mapreduce/WordCountDriver.java
@@ -22,6 +22,22 @@ import java.io.IOException;
 @SuppressWarnings("all")
 public class WordCountDriver {
 
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration configuration = new Configuration();
+        Job job = Job.getInstance(configuration);
+        job.setJarByClass(WordCountDriver.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        boolean result = job.waitForCompletion(true);
+        System.exit(result ? 0 : 1);
+    }
+
     @Test
     public void go() throws IOException, ClassNotFoundException, InterruptedException {
         Configuration configuration = new Configuration();
